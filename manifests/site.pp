3d7b13384153f0bf9c10ebfe8c9475b70ff5eba6
Les Aker <me@lesaker.org>
2022-07-26 22:35:17 -0400

    add sites config

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,13 @@
 # @summary Configure nginx site
 #
+# @param proxy_target sets the target for the nginx proxy
+# @param bind_address sets the IP for the site
+# @param allow_ranges restricts access to the site based on source IP
 # @param site sets the name of the site
 define nginx::site (
+  String $proxy_target,
+  String $bind_address = '[::]',
+  Array[String] $allow_ranges = [],
   String $site = $title,
 ) {
   file { "/etc/nginx/${site}.conf":
