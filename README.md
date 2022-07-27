da5ef3bfc651584250369e96d47b9ae0cfb76310
Les Aker <me@lesaker.org>
2022-07-26 22:00:43 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+puppet-nginx
+===========
+
+[![Puppet Forge](https://img.shields.io/puppetforge/v/halyard/nginx.svg)](https://forge.puppetlabs.com/halyard/nginx)
+[![MIT Licensed](http://img.shields.io/badge/license-MIT-green.svg?style=flat)](https://tldrlegal.com/license/mit-license)
+
+Module to configure nginx sites
+
+## Usage
+
+To install nginx:
+
+```puppet
+include nginx
+```
+
---
+++ manifests/init.pp
@@ -0,0 +1,36 @@
+# @summary Configure nginx sites
+#
+# @param sites
+class nginx (
+  Hash[String, Hash[String, Any]] $sites = {},
+) {
+  package { 'nginx': }
+
+  -> file { [
+      '/etc/nginx',
+      '/etc/nginx/sites',
+    ]:
+      ensure => directory,
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0755',
+  }
+
+  file { '/etc/nginx/nginx.conf':
+    ensure => file,
+    source => 'puppet:///modules/nginx/nginx.conf',
+    notify => Service['nginx'],
+  }
+
+  $sites.each |String $site, Hash $config| {
+    nginx::site { $site:
+      *      => $config,
+      notify => Service['nginx'],
+    }
+  }
+
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+  }
+}
