242f5bd0fae439c22e5e978a07c9e0b7c2300ce2
Les Aker <me@lesaker.org>
2022-07-26 23:23:22 -0400

    update params

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,14 +3,14 @@
 # @param proxy_target sets the target for the nginx proxy
 # @param tls_account sets the TLS account config
 # @param tls_challengealias sets the alias for TLS cert
-# @param bind_address sets the IP for the site
+# @param bind_addresses sets the IP for the site
 # @param allow_ranges restricts access to the site based on source IP
 # @param site sets the name of the site
 define nginx::site (
   String $proxy_target,
   String $tls_account,
   Optional[String] $tls_challengealias = undef,
-  String $bind_address = '[::]',
+  Array{String} $bind_addresses = ['*', '[::]'],
   Array[String] $allow_ranges = [],
   String $site = $title,
 ) {
