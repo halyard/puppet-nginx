372540540447a8412a4ea1e6a9b899f87b11c753
Les Aker <me@lesaker.org>
2022-07-27 09:25:37 -0400

    update to enable controlling the CSP

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,6 +5,7 @@
 # @param tls_challengealias sets the alias for TLS cert
 # @param bind_addresses sets the IP for the site
 # @param allow_ranges restricts access to the site based on source IP
+# @param csp sets the content security policy for the site
 # @param site sets the name of the site
 define nginx::site (
   String $proxy_target,
@@ -12,6 +13,7 @@ define nginx::site (
   Optional[String] $tls_challengealias = undef,
   Array[String] $bind_addresses = ['*', '[::]'],
   Array[String] $allow_ranges = [],
+  String $csp = "default-src 'self' http: https: ws: wss: data: blob: 'unsafe-inline'; frame-ancestors 'self';",
   String $site = $title,
 ) {
   acme::certificate { $site:
